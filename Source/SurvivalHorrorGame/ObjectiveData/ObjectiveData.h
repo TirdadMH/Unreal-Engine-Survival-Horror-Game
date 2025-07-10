449ccea7be5d7d9ee4dec3bc5f56526f130b54ab
Mohammad Moazzen <mohamma.moa1381@gmail.com>
2025-07-10 13:15:28 +0330

    Added C++ files to project

---
+++ Source/SurvivalHorrorGame/ObjectiveData/ObjectiveData.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StructUtils/UserDefinedStruct.h"
+#include "ObjectiveData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURVIVALHORRORGAME_API UObjectiveData : public UUserDefinedStruct
+{
+	GENERATED_BODY()
+	
+	
+};
